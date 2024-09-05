9bda7e9f47f9ad8d73e1911e2169b3ca1c7301b4
ADMIN <khiemnguyenviet.2004@gmail.com>
2024-09-05 10:41:59 +0700

    khiem commit

--- src/main/java/com/example/storeproject/Controller/HomeController.java
+++ src/main/java/com/example/storeproject/Controller/HomeController.java
@@ -1,11 +1,33 @@
 package com.example.storeproject.Controller;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-
+@Controller
 public class HomeController {
-    @RequestMapping("/home")
+
+    @RequestMapping("home")
     public String home(Model model){
         return "home";
+    }
+
+    @RequestMapping("login")
+    public String login(Model model){
+        return "login";
+    }
+
+    @RequestMapping("signup")
+    public String signup(Model model){
+        return "signup";
+    }
+
+    @RequestMapping("product_detail")
+    public String product_detail(Model model){
+        return "product_detail";
+    }
+
+    @RequestMapping("store")
+    public String store(Model model){
+        return "store";
     }
 }
